0531c6b9516e9e3d4d4b8563f3c860babc2d4ebb
LAKSHMI-PARMALLA <lakshmiparmalla@gmail.com>
2023-11-02 17:55:14 +0530

    Basic Programs

--- CountDigits.java
+++ CountDigits.java
@@ -6,12 +6,13 @@ public class CountDigits {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter a number");
        int a = sc.nextInt();
+       int temp = a;
        int numOfDigits = 0;
        while(a != 0)
        {
            a = a/10;
            numOfDigits++;
        }
-        System.out.println("Number of digits in " +a+" is " +numOfDigits);
+        System.out.println("Number of digits in "+temp+" is " +numOfDigits);
     }
 }
